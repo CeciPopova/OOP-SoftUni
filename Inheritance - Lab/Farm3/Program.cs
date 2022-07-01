acc776c985cb697a927886de3f840a9d16beeca6
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-01 13:31:24 +0300

    Add new Folder

---
+++ Inheritance - Lab/Farm3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+            dog.Eat();
+            dog.Bark();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
