88aef6ff084617a7c724960fff2605d79bb01f61
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-02 10:56:07 +0300

    Add new Folder

---
+++ Inheritance - Lab/P04. Random List/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("Pesho");
+            list.Add("Pesho1");
+            list.Add("Pesho2");
+            list.Add("Pesho3");
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
---
+++ Inheritance - Lab/P04. Random List/RandomList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rand = new Random();
+            if (true)
+            {
+                if (Count == 0)
+                {
+                    return null;
+                }
+                var index = rand.Next(0, base.Count);
+                var element = base[index];
+                base.RemoveAt(index);
+                return element;
+            }
+        }
+
+
+    }
+}
