a9d4c3a4d0b48342609944a966375d2a980ef2b5
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-02 11:11:47 +0300

    Add new Folder

---
+++ Inheritance - Lab/P05. Stack Of Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            stackOfStrings.AddRange(new List<string>() { "1", "2", "3" });
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            Console.WriteLine(stackOfStrings.Pop());
+            Console.WriteLine(stackOfStrings.Pop());
+        }
+    }
+}
---
+++ Inheritance - Lab/P05. Stack Of Strings/StackOfStrings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Stack<string> AddRange(IEnumerable<string> elements)
+        {
+            foreach (var item in elements)
+            {
+                Push(item);
+            }
+            return this;
+        }
+    }
+}
