eadc0f31bd9b81b3154243111f9f28d7f6ab3014
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-03 21:57:07 +0300

    Add new Folder

---
+++ Inheritance - Exercise/P02.Zoo/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+    }
+}
