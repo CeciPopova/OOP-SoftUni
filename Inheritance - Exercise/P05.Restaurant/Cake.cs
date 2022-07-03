17485e811e953529c84810b910bca81a343dd364
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-03 08:59:41 +0300

    Add new Folder

---
+++ Inheritance - Exercise/P05.Restaurant/Cake.cs
@@ -0,0 +1,15 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double DefGrams = 250;
+        private const double DefCalories = 1000;
+        private const decimal CakePrice = 5m;
+
+        public Cake(string name)
+            : base(name, CakePrice, DefGrams, DefCalories)
+        {
+
+        }
+    }
+}
