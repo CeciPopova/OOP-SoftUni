4a75a80a5c28c69ad205a02bb6854a391a5b3839
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-03 23:03:12 +0300

    Add new Folder

---
+++ Inheritance - Exercise/P04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+
+        public double DefaultFuelConsumption { get; set; } = 1.25;
+        public virtual double FuelConsumption { get => DefaultFuelConsumption; }
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+
+        }
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel = this.Fuel - FuelConsumption * kilometers;
+        }
+        public override string ToString()
+        {
+            return $"HorsePower: {this.HorsePower} Fuel: {this.Fuel}";
+        }
+    }
+}
