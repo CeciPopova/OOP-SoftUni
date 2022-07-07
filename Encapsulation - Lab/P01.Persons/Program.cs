825f6ea6363e27f3359ece71816ebb4fdecea0c1
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-07 17:54:18 +0300

    Add new Folder

---
+++ Encapsulation - Lab/P01.Persons/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] cmndArgs = Console.ReadLine().Split(' ');
+                Person person = new Person(cmndArgs[0], cmndArgs[1], int.Parse(cmndArgs[2]));
+                people.Add(person);
+            }
+            people.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString())); ;
+
+        }
+    }
+}
