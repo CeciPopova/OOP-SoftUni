3bc876c74b27eea2b43a79c342c772d9ef313692
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-08 10:08:20 +0300

    Add new Folder

---
+++ Encapsulation - Exercise/P02.AnimalFarm/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AnimalFarm
+{
+    using AnimalFarm.Models;
+    using System;
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Chicken chicken;
+            try
+            {
+                chicken = new Chicken(name, age);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.ProductPerDay);
+        }
+    }
+}
