a2a62128b955786e83b2d6c8a9049ac2ef7226f5
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-10 13:10:53 +0300

    Add new Folder

---
+++ Interfaces and Abstraction - Lab/P01.Shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; ++i)
+                DrawLine(this.width, '*', ' ');
+            DrawLine(this.width, '*', '*');
+        }
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; ++i)
+                Console.Write(mid);
+            Console.WriteLine(end);
+        }
+
+    }
+}
