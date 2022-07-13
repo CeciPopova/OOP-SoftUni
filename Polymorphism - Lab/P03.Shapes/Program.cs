aa129c50e06f223757e41bb4264ca90207239247
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-13 17:24:24 +0300

    Add new Project

---
+++ Polymorphism - Lab/P03.Shapes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Shapes
+{
+    using System;
+    public class StartUp
+    {
+        static void Main()
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double radius = double.Parse(Console.ReadLine());
+
+            Shape shape;
+
+            shape = new Rectangle(height, width);
+
+            if (shape is Rectangle)
+            {
+                Console.WriteLine(shape.CalculateArea());
+                Console.WriteLine(shape.CalculatePerimeter());
+                Console.WriteLine(shape.Draw());
+            }
+            shape = new Circle(radius);
+
+            if (shape is Circle)
+            {
+                Console.WriteLine(shape.CalculateArea());
+                Console.WriteLine(shape.CalculatePerimeter());
+                Console.WriteLine(shape.Draw());
+            }
+        }
+    }
+}
---
+++ Polymorphism - Lab/P03.Shapes/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                height = value;
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                width = value;
+            }
+        }
+        public override double CalculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (this.Width + this.Height);
+        }
+
+        public sealed override string Draw()
+        {
+            return $"{base.Draw()} Rectangle";
+        }
+    }
+}
