8ecaca384df6f3003794122e192ed30acccf59d1
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-23 13:06:35 +0300

    Add new Project

---
+++ 09. Reflection and Attributes - Lab/P01.Stealer/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] classFields = classType.GetFields(
+                BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {investigatedClass}");
+            foreach (var item in classFields.Where(i => requestedFields.Contains(i.Name)))
+            {
+                sb.AppendLine($"{item.Name} = {item.GetValue(classInstance)}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
