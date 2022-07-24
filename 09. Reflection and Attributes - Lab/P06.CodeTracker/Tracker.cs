e16125e26a2b3cfc5a051e3055f4008918471464
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-24 10:58:30 +0300

    Add new Project

---
+++ 09. Reflection and Attributes - Lab/P06.CodeTracker/Tracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+
+            var type = typeof(StartUp);
+
+            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute a in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {a.Name}");
+                    }
+                }
+            }
+
+        }
+    }
+}
