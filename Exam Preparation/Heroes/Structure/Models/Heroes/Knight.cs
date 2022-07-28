cb44eb60395233d2f00b63552c28bce72bd89c7b
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-28 16:29:47 +0300

    Delete Folder

--- Exam Preparation/Heroes/Structure/Models/Heroes/Knight.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Heroes.Models.Heroes
-{
-    public class Knight : Hero
-    {
-        public Knight(string name, int health, int armour) : base(name, health, armour)
-        {
-        }
-    }
-}
