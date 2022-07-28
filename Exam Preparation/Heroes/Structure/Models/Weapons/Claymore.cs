d73ba7e94ec309a0fe9f9f03944e858f3f94a1d3
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-07-28 16:31:55 +0300

    Add new Project

---
+++ Exam Preparation/Heroes/Structure/Models/Weapons/Claymore.cs
@@ -0,0 +1,23 @@
+﻿namespace Heroes.Models.Weapons
+{
+    public class Claymore : Weapon
+    {
+        private int damage;
+        public Claymore(string name, int durability) : base(name, durability)
+        {
+            damage = 20;
+        }
+
+        public override int DoDamage()
+        {
+            Durability--;
+            if (Durability == 0)
+            {
+                return 0;
+            }
+            return damage;
+        }
+
+    }
+}
+
