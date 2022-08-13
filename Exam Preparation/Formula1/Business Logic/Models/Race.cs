2d13871616d6a62436e16a3604bf48b5b75a3577
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-08-13 09:12:07 +0300

    Add new Folder

---
+++ Exam Preparation/Formula1/Business Logic/Models/Race.cs
@@ -0,0 +1,89 @@
+﻿using Formula1.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class Race : IRace
+    {
+        private string raceName;
+        private int numberOfLaps;
+        private bool tookPlace;
+        private ICollection<IPilot> pilots;
+
+
+
+        public Race(string raceName, int numberOfLabs)
+        {
+            this.RaceName = raceName;
+            this.NumberOfLaps = numberOfLabs;
+            this.TookPlace = false;
+            pilots = new List<IPilot>();
+
+        }
+        public string RaceName
+        {
+            get { return raceName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException($"Invalid race name: {raceName}.");
+                }
+                raceName = value;
+            }
+        }
+
+        public int NumberOfLaps
+        {
+            get { return numberOfLaps; }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException($"Invalid lap numbers: {numberOfLaps}.");
+                }
+                numberOfLaps = value;
+            }
+        }
+
+
+        public bool TookPlace
+        {
+            get { return tookPlace; }
+            set { tookPlace = value; }
+
+        }
+
+
+        public ICollection<IPilot> Pilots
+        {
+            get { return pilots; }
+
+        }
+
+
+
+        public void AddPilot(IPilot pilot)
+        {
+
+            //if (pilots.Contains(pilot))
+            //{
+            //    throw new NotImplementedException();
+            //}
+            pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The {RaceName} race has:");
+            sb.AppendLine($"Participants: {Pilots.Count}");
+            sb.AppendLine($"Number of laps: {NumberOfLaps}");
+            sb.Append(TookPlace == true ? $"Took place: Yes" : $"Took place: No");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
