ffd4d8721a168e102af97dee9d7503284db9d191
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-08-13 09:10:53 +0300

    Delete Folder

--- Exam Preparation/Formula1/Busines Logic/Models/Williams.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Formula1.Models
-{
-    public class Williams : FormulaOneCar
-    {
-        public Williams(string model, int horsepower, double engineDisplacement) : base(model, horsepower, engineDisplacement)
-        {
-        }
-    }
-}
