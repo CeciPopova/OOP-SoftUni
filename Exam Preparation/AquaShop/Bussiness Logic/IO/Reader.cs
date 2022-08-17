e43d3a91a592caffb6e036dfe7825026b320ec98
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-08-17 18:15:38 +0300

    Add new Folder

---
+++ Exam Preparation/AquaShop/Bussiness Logic/IO/Reader.cs
@@ -0,0 +1,13 @@
+﻿namespace AquaShop.IO
+{
+    using AquaShop.IO.Contracts;
+    using System;
+
+    public class Reader : IReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ Exam Preparation/AquaShop/Bussiness Logic/Utilities/Messages/OutputMessages.cs
@@ -0,0 +1,15 @@
+﻿namespace AquaShop.Utilities.Messages
+{
+    public static class OutputMessages
+    {
+        public const string SuccessfullyAdded = "Successfully added {0}.";
+
+        public const string EntityAddedToAquarium = "Successfully added {0} to {1}.";
+
+        public const string UnsuitableWater = "Water not suitable.";
+
+        public const string FishFed = "Fish fed: {0}";
+
+        public const string AquariumValue = "The value of Aquarium {0} is {1:f2}.";
+    }
+}
