217737d22f6ede491a0897417729cdd5a3663794
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-08-20 11:48:24 +0300

    Add new Project

---
+++ Exam Preparation/NavalVessels/Business Logic/Models/Battleship.cs
@@ -0,0 +1,56 @@
+﻿using NavalVessels.Models.Contracts;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Battleship : Vessel, IBattleship
+    {
+        private const double InitialArmorThickness = 300;
+
+        public Battleship(string name, double mainWeaponCaliber, double speed)
+            : base(name, mainWeaponCaliber, speed, InitialArmorThickness)
+        {
+        }
+
+        public bool SonarMode { get; private set; } = false;
+
+        public override void RepairVessel()
+        {
+            if (this.ArmorThickness < InitialArmorThickness)
+            {
+                this.ArmorThickness = InitialArmorThickness;
+            }
+        }
+
+        public void ToggleSonarMode()
+        {
+            if (SonarMode == true)
+            {
+                SonarMode = false;
+
+                this.MainWeaponCaliber -= 40;
+                this.Speed += 5;
+            }
+            else
+            {
+                SonarMode = true;
+
+                this.MainWeaponCaliber += 40;
+                this.Speed -= 5;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string sonarModeString = this.SonarMode == true ? "ON" : "OFF";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+            sb.AppendLine($" *Sonar mode: {sonarModeString}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exam Preparation/NavalVessels/Business Logic/Models/Submarine.cs
@@ -0,0 +1,56 @@
+﻿using NavalVessels.Models.Contracts;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Submarine : Vessel, ISubmarine
+    {
+        private const double InitialArmorThickness = 200;
+
+        public Submarine(string name, double mainWeaponCaliber, double speed)
+            : base(name, mainWeaponCaliber, speed, InitialArmorThickness)
+        {
+        }
+
+
+        public bool SubmergeMode { get; private set; } = false;
+
+        public override void RepairVessel()
+        {
+            if (this.ArmorThickness < InitialArmorThickness)
+            {
+                this.ArmorThickness = InitialArmorThickness;
+            }
+        }
+
+        public void ToggleSubmergeMode()
+        {
+            if (this.SubmergeMode == false)
+            {
+                this.SubmergeMode = true;
+
+                this.MainWeaponCaliber += 40;
+                this.Speed -= 4;
+            }
+            else
+            {
+                this.SubmergeMode = false;
+
+                this.MainWeaponCaliber -= 40;
+                this.Speed += 4;
+            }
+        }
+
+        public override string ToString()
+        {
+            string submergeModeString = this.SubmergeMode == true ? "ON" : "OFF";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+            sb.AppendLine($" *Submerge mode: {submergeModeString}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
